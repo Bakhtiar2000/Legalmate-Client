4e9607602140006c4f38cee240fa146db0490faf
radifkhanrafin <meherafkabir@gmail.com>
2023-10-31 08:02:51 +0600

    review

--- src/pages/attorneys/AttorneyReviews.jsx
+++ src/pages/attorneys/AttorneyReviews.jsx
@@ -9,29 +9,45 @@ import useAuth from '../../hooks/useAuth';
 import moment from 'moment/moment';
 import { useForm } from 'react-hook-form';
 import RatingComponent from '../../components/RatingComponent';
+import useAxiosSecure from '../../hooks/useAxios';
 
-const AttorneyReviews = ({ reviews, name }) => {
-    const{currentUser}= useAuth()
-    const [isReviewClicked, setIsReviewClicked]= useState(false)
+const AttorneyReviews = ({ paymentSuccess, reviews, email, name }) => {
+    const { currentUser } = useAuth();
+    const [axiosSecure] = useAxiosSecure();
+    const [isReviewClicked, setIsReviewClicked] = useState(false);
     const [userRating, setUserRating] = useState(0);
-    const { register, handleSubmit, reset} = useForm();
+    const { register, handleSubmit, reset } = useForm();
 
-    const onReviewSubmit= data =>{
-        const date= moment(new Date()).format("MMMM D, YYYY")
-        const name= currentUser.name
-        const addedReview= {
+    const onReviewSubmit = data => {
+        const date = moment(new Date()).format("MMMM D, YYYY")
+        const name = currentUser.name
+        const addedReview = {
             name,
             date,
             review: data.review,
             rating: userRating
 
         }
-        console.log(addedReview);
+        const newReview = [...reviews, addedReview]
+        const updateData = {
+            email: email,
+            newReview
+        }
+        console.log(updateData);
+        axiosSecure.patch('/attorney/review', updateData)
+            .then(res => {
+                if (res.status === 200) {
 
+                    setIsReviewClicked(false)
+                    setUserRating(0)
+                    reset()
+                }
+            })
+            .catch(error => {
+                console.log(error);
+            })
         //TODO: save the basic info data and refetch() after sending to database
-        setIsReviewClicked(false)
-        setUserRating(0)
-        reset()
+
     }
 
     // rating style
@@ -50,89 +66,92 @@ const AttorneyReviews = ({ reviews, name }) => {
         <div className='p-5 rounded-lg bg-lightDark'>
             <Swiper
                 pagination={{
-                clickable: true,
+                    clickable: true,
                 }}
                 breakpoints={{
                     768: {
-                      slidesPerView: 1,
-                      spaceBetween: 40,
+                        slidesPerView: 1,
+                        spaceBetween: 40,
                     },
                     1024: {
                         slidesPerView: 2,
                         spaceBetween: 50,
                     }
-                  }}
+                }}
                 modules={[Pagination]}
                 className="mySwiper"
             >
 
                 {
-                    reviews.map((review, index)=> 
-                    <SwiperSlide key={index}>
-                        <div className='duration-300 h-full bg-lightDark rounded-lg p-3 md:py-5 md:px-8 border border-slate-500 shadow-lg max-w-4xl mx-auto'>
-                           <div className='flex items-center justify-between gap-5 mb-5'>
-                                <div>
-                                    <p className='text-xl md:text-2xl font-semibold md:mb-2'>{review?.name}</p>
-                                    <p className='text-slate-500'>{review?.date}</p>
+                    reviews.map((review, index) =>
+                        <SwiperSlide key={index}>
+                            <div className='duration-300 h-full bg-lightDark rounded-lg p-3 md:py-5 md:px-8 border border-slate-500 shadow-lg max-w-4xl mx-auto'>
+                                <div className='flex items-center justify-between gap-5 mb-5'>
+                                    <div>
+                                        <p className='text-xl md:text-2xl font-semibold md:mb-2'>{review?.name}</p>
+                                        <p className='text-slate-500'>{review?.date}</p>
+                                    </div>
+                                    {/* rating */}
+                                    <Rating
+                                        className="max-w-[110px]"
+                                        readOnly
+                                        value={review?.rating}
+                                        itemStyles={myStyles}
+                                    />
                                 </div>
-                                {/* rating */}
-                                <Rating
-                                    className="max-w-[110px]"
-                                    readOnly
-                                    value={review?.rating}
-                                    itemStyles={myStyles}
-                                />
-                           </div>
-    
-                            <p className='mb-5 italic'>"{review?.review}"</p>
-                        </div>
-                    </SwiperSlide>)
+
+                                <p className='mb-5 italic'>"{review?.review}"</p>
+                            </div>
+                        </SwiperSlide>)
                 }
 
             </Swiper>
 
             <form className='flex gap-5 mt-5' onSubmit={handleSubmit(onReviewSubmit)}>
                 <div className='w-full text-black'>
-                {
-                    isReviewClicked &&
-                    <>
-                    <div className='flex items-center gap-2 mb-1'>
-                        <h2 className='text-white font-semibold'>Rate the Attorney:</h2>
-                        <RatingComponent initialRating={userRating} onRatingChange={handleRatingChange} />
-                    </div>
-                    <textarea 
-                        {...register("review")}
-                        placeholder={`Write your experience with ${name} within 150 words`}
-                        className='w-full h-20 border border-dark/40 p-2 rounded-md focus:outline-none focus:border-primary mb-1 sm:mb-3'
-                    />
-                    </>
-                }
+                    {
+                        isReviewClicked &&
+                        <>
+                            <div className='flex items-center gap-2 mb-1'>
+                                <h2 className='text-white font-semibold'>Rate the Attorney:</h2>
+                                <RatingComponent initialRating={userRating} onRatingChange={handleRatingChange} />
+                            </div>
+                            <textarea
+                                {...register("review")}
+                                placeholder={`Write your experience with ${name} within 150 words`}
+                                className='w-full h-20 border border-dark/40 p-2 rounded-md focus:outline-none focus:border-primary mb-1 sm:mb-3'
+                            />
+                        </>
+                    }
                 </div>
 
 
+                
                 <div className='flex justify-end mt-3'>
-                {
-                    !isReviewClicked?
-                    <button 
-                        onClick={()=> setIsReviewClicked(true)}
-                        className="mt-2 w-max text-center px-3 py-1 bg-secondary hover:bg-secondary/60 duration-300 rounded text-white cursor-pointer"
-                    >
-                        Write a Review
-                    </button>:
-                    <div className='flex justify-end gap-2 items-center'>
-                        <button 
-                        onClick={()=> setIsReviewClicked(false)} 
-                        className="mt-2 w-fit text-center px-3 py-1 bg-red-500 hover:bg-red-500/60 duration-300 rounded text-white cursor-pointer"
-                        >
-                            Cancel
-                        </button>
-                        <input
-                            type='submit' 
-                            value="Save" 
-                            className="mt-2 w-fit text-center px-3 py-1 bg-green-500 hover:bg-green-500/60 duration-300 rounded text-white cursor-pointer"
-                        />
-                    </div>
-                }
+                    {
+                        !isReviewClicked ?
+
+                            <button
+                                onClick={() => setIsReviewClicked(true)}
+                                className="mt-2 w-max text-center px-3 py-1 bg-secondary hover:bg-secondary/60 duration-300 rounded text-white cursor-pointer"
+                                disabled={!paymentSuccess}
+                            >
+                                Write a Review
+                            </button> :
+                            <div className='flex justify-end gap-2 items-center'>
+                                <button
+                                    onClick={() => setIsReviewClicked(false)}
+                                    className="mt-2 w-fit text-center px-3 py-1 bg-red-500 hover:bg-red-500/60 duration-300 rounded text-white cursor-pointer"
+                                >
+                                    Cancel
+                                </button>
+                                <input
+                                    type='submit'
+                                    value="Save"
+                                    className="mt-2 w-fit text-center px-3 py-1 bg-green-500 hover:bg-green-500/60 duration-300 rounded text-white cursor-pointer"
+                                />
+                            </div>
+                    }
                 </div>
             </form>
         </div>
